96a9fb6a3ef7250b2a87f7a30b072d9c7b1ba893
DESKTOP-V2HE1AF\elliot <emily.chen@tpisoftware.com>
2021-09-22 14:46:12 +0800

    初始化

---
+++ demo/src/main/java/com/coin/entity/Coin.java
@@ -0,0 +1,33 @@
+package com.coin.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Coin {
+
+	 	@Id
+	    private Long id;
+	    private String code;
+	    private String chineseName;
+	    
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getCode() {
+			return code;
+		}
+		public void setCode(String code) {
+			this.code = code;
+		}
+		public String getChineseName() {
+			return chineseName;
+		}
+		public void setChineseName(String chineseName) {
+			this.chineseName = chineseName;
+		}
+	    
+}
