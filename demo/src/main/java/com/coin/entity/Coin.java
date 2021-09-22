2be05b6e79458826e0d0f9b9f4ccbc57cdf70e35
wenChen1030 <emily.chen@tpisoftware.com>
2021-09-22 16:43:55 +0800

    test

--- demo/src/main/java/com/coin/entity/Coin.java
+++ demo/src/main/java/com/coin/entity/Coin.java
@@ -11,23 +11,5 @@ public class Coin {
 	    private String code;
 	    private String chineseName;
 	    
-		public Long getId() {
-			return id;
-		}
-		public void setId(Long id) {
-			this.id = id;
-		}
-		public String getCode() {
-			return code;
-		}
-		public void setCode(String code) {
-			this.code = code;
-		}
-		public String getChineseName() {
-			return chineseName;
-		}
-		public void setChineseName(String chineseName) {
-			this.chineseName = chineseName;
-		}
 	    
 }
